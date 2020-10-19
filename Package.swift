88b2b2f0a47203b0b6a0eeb0832b1c46f79ec616
Juan José Lovera <juanLovera@users.noreply.github.com>
2020-10-19 12:57:34 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,10 +14,10 @@ let package = Package(
             targets: ["QueuesFluentDriver"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.32.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/sql-kit.git", from: "3.6.0"),
-        .package(url: "https://github.com/vapor/queues.git", from: "1.1.0"),
+        .package(url: "https://github.com/vapor/queues.git", from: "1.5.0"),
     ],
     targets: [
         .target(
